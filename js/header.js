ed77192282649a97330a5edaf98a014598306159
Carlos Carneglia <cd.carneglia@gmail.com>
2023-10-09 19:39:43 -0300

    some styles added

--- js/header.js
+++ js/header.js
@@ -5,7 +5,7 @@ menuList.style.height = "0px";
 function togglemenu() {
 
     if (menuList.style.height == "0px") {
-        menuList.style.height = "230px";
+        menuList.style.height = "250px";
     }
     else {
         menuList.style.height = "0px";
--- js/index.js
+++ js/index.js
@@ -1,5 +1,15 @@
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
 
-$(document).ready(function () {
-           
-    $("#myCarousel").carousel({ pause: false });
-})
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        }
+    }
+}
+
+window.addEventListener("scroll", reveal);
